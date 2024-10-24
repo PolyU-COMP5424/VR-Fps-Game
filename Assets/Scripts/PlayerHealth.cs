21c5a26562bb946525110ea1c8bd7614fcc616b4
cf21683 <98465390+cf21683@users.noreply.github.com>
2024-10-24 21:31:56 +0800

    finish the project

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public GameObject camera;
+
+    public int hp = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void underacctick(int d) 
+    { 
+        hp-=d;
+        if (hp < 0){
+            camera.SetActive(true);
+            Destroy(gameObject);
+        };
+        Debug.Log(hp);
+    }
+}
